caffb5688c8353db2bdc8ce46fb29b73fde5cffe
Reagan Yego <reaganyego@gmail.com>
2023-06-21 12:41:36 -0700

    printing all lowercase letters

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,7 @@
 void print_alphabet(void)
 {
 	char letter;
-
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
-
-
-	_putchar('\n');
+	for(letter="a", letter="z", letter++)
+		_putchar(letter);
+	_putchar("\n");
 }
-
